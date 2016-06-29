a901e1563598967e812c7a3aa8fb17fd3d21f3aa
Alexander Kislitsky <akislitsky@mirantis.com>
2016-06-29 14:31:13 +0300

    Initiall commit
    Keys paths extracting implemented

---
+++ sketch/collection_utils/__init__.py
@@ -0,0 +1,54 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def extract_key_paths(base_path, collection):
+    """Extracts key paths for collection and joins with base_path.
+
+    If collection is list or tuple indexes will be added to base_path.
+    If collection is dict keys will be added to base_path.
+
+    :param base_path: list of indexes
+    :param collection: collection
+    :return: base_path extended by collection indexes
+    """
+
+    if isinstance(collection, dict):
+        return [base_path + [k] for k in collection.keys()]
+    elif isinstance(collection, (list, tuple)):
+        return [base_path + [idx] for idx in range(len(collection))]
+    else:
+        return None
+
+
+def collect_all_paths(collection):
+    """Extracts all paths to leaf elements of collection.
+
+    In case of {'a': {'b': 1}} result will be [['a', 'b']]
+    In case of ['a', {'b': 1}}] result will be [[0], [1, 'b']]
+    Empty collection considered as leaf element and in this case
+    for {'a': []} result will be [['a']]
+
+    :param collection: collection
+    :return: list of paths to leaf elements
+    """
+
+    paths = extract_key_paths([], collection)
+    for path in paths:
+        point = collection
+        for k in path:
+            point = point[k]
+        new_paths = extract_key_paths(path, point)
+        if new_paths:
+            paths.extend(new_paths)
+        else:
+            yield path
---
+++ sketch/collection_utils/test/test_collection_utils.py
@@ -0,0 +1,75 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import unittest2
+
+from sketch import collection_utils as cu
+
+
+class TestKeysPathsFinding(unittest2.TestCase):
+
+    def test_extract_key_paths(self):
+        self.assertEqual([], cu.extract_key_paths([], {}))
+        self.assertEqual([], cu.extract_key_paths([], []))
+        self.assertEqual([], cu.extract_key_paths([], ()))
+        self.assertItemsEqual(
+            [['a'], ['b'], [1]],
+            cu.extract_key_paths([], {'a': 1, 'b': {'bb': 10}, 1: 2})
+        )
+        self.assertItemsEqual(
+            [[0], [1], [2]],
+            cu.extract_key_paths([], ['a', {}, 3])
+        )
+        self.assertItemsEqual(
+            [['x', 'y', 'a'], ['x', 'y', 'b']],
+            cu.extract_key_paths(['x', 'y'], {'a': 1, 'b': []})
+        )
+        self.assertItemsEqual(
+            [['x', 'y', 'a'], ['x', 'y', 'b']],
+            cu.extract_key_paths(['x', 'y'], {'a': 1, 'b': []})
+        )
+        self.assertIsNone(cu.extract_key_paths([], set([1, 2])))
+        self.assertIsNone(cu.extract_key_paths([], frozenset([1, 2])))
+        self.assertItemsEqual(
+            [[0], [1]],
+            cu.extract_key_paths([], (1, 2))
+        )
+        self.assertItemsEqual(
+            [[0], [1]],
+            cu.extract_key_paths([], (1, {'a': 'a_val'}))
+        )
+
+    def test_collect_all_paths(self):
+        self.assertItemsEqual([], cu.collect_all_paths([]))
+        self.assertItemsEqual([
+            [0], [1], [2], [3, 0]],
+            cu.collect_all_paths([1, [], {}, [1]])
+        )
+        self.assertItemsEqual([], cu.collect_all_paths([]))
+        self.assertItemsEqual(
+            [['a'], ['b'], ['c', 'd']],
+            cu.collect_all_paths({'a': {}, 'b': [], 'c': {'d': 1}})
+        )
+        self.assertItemsEqual(
+            [['a'], ['b'], ['c', 'd']],
+            cu.collect_all_paths({'a': 1, 'b': 2, 'c': {'d': 3}})
+        )
+        self.assertItemsEqual([[0]], cu.collect_all_paths({0: None}))
+        self.assertItemsEqual([[0], [1]], cu.collect_all_paths((1, 2)))
+        self.assertItemsEqual(
+            [[0], [1, 'a'], [1, 'b', 0]],
+            cu.collect_all_paths((1, {'a': 1, 'b': [2]}))
+        )
+        self.assertItemsEqual(
+            [['a'], ['b', 0]],
+            cu.collect_all_paths({'a': 1, 'b': (2,)})
+        )
